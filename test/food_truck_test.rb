adf2f5100773a34841c851eff04aa0190adcb7e6
Antonio Jackson <antoniojackson@gmail.com>
2020-04-15 10:21:24 -0600

    Adding FoodTruck tests

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -1,3 +1,42 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/item'
 require './lib/food_truck'
+
+class FoodTruckTest < Minitest::Test
+
+  def setup
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+  end
+
+  def test_it_exists
+    assert_instance_of FoodTruck, @food_truck
+  end
+
+  def test_it_has_attributes
+    assert_equal "Rocky Mountain Pies", @food_truck.name
+    assert_equal ({}), @food_truck.inventory
+  end
+
+  def test_check_stock
+    assert_equal 0, @food_truck.check_stock(@item1)
+  end
+
+  def test_stock
+    @food_truck.stock(@item1, 30)
+    assert_equal 30, @food_truck.check_stock(@item1)
+    @food_truck.stock(@item1, 25)
+    assert_equal 55, @food_truck.check_stock(@item1)
+  end
+
+  def test_inventory
+    @food_truck.stock(@item1, 30)
+    assert_equal ({@item1 => 30}), @food_truck.inventory
+    @food_truck.stock(@item1, 25)
+    @food_truck.stock(@item2, 12)
+    result = {@item1 => 55, @item2 => 12}
+    assert_equal result, @food_truck.inventory
+  end
+end
