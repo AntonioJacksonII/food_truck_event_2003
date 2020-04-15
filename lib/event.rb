9c555fb7d1fb9adda96dbcf0513c6c8224e8846e
Antonio Jackson <antoniojackson@gmail.com>
2020-04-15 11:58:32 -0600

    Final commit

--- lib/event.rb
+++ lib/event.rb
@@ -19,4 +19,22 @@ class Event
       truck.inventory[item] > 0
     end
   end
+
+  def total_inventory
+    inventory_total = {}
+    @food_trucks.each do |truck|
+      truck.inventory.each do |items|
+        items.each do |item|
+          if inventory_total[items[0]].nil?
+            inventory_total[items[0]] = {quantity: @food_trucks.sum {|truck| truck.check_stock(items[0])},
+                                        food_trucks: [truck]}
+          else
+            inventory_total[items[0]] = {quantity: @food_trucks.sum {|truck| truck.check_stock(items[0])},
+                                          food_trucks: [truck]}
+          end
+        end
+      end
+    end
+    inventory_total
+  end
 end
